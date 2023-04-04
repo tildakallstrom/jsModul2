d3c4fc289031eaab1fe43bf30f1cf4cbb10157a8
Tilda Källström <tildakallstrom@gmail.com>
2023-04-04 18:09:23 +0200

    Added exercise 2.3

--- js/ovning22.js
+++ js/ovning22.js
@@ -6,14 +6,18 @@ function add(num1, num2) {
     return num1 * num2;
   }
   
+  function secondSum(sum, multiplySum) {
+    return sum + multiplySum;
+  }
+  
   function calculate() {
     let num1 = Number(document.getElementById("num1").value);
     let num2 = Number(document.getElementById("num2").value);
     
     let sum = add(num1, num2);
     let multiplySum = multiply(num1, num2);
+    let resultSum = secondSum(sum, multiplySum);
     
     let resultElement = document.getElementById("result");
-    resultElement.innerHTML = "Sum: " + sum + "<br>Product: " + multiplySum;  
-}
-  
+    resultElement.innerHTML = "Sum: " + sum + "<br>Product: " + multiplySum + "<br>Sum of sum: " + resultSum;  
+  }  
