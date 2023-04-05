b3801b475d418514f2d24664961458f27e7d511d
Tilda Källström <tildakallstrom@gmail.com>
2023-04-05 11:55:44 +0200

    Added exercise 2.12 and 2.13

---
+++ js/ovning212.js
@@ -0,0 +1,10 @@
+const str = "Hej på dig!";
+
+const subStr = str.slice(4, 7);
+console.log(subStr); 
+
+const wordss = str.split(" ");
+console.log(wordss); 
+
+const dashedWords = wordss.join("-");
+console.log(dashedWords);
---
+++ js/ovning213.js
@@ -0,0 +1,13 @@
+const strr = "Hej på dig!";
+
+const indexOfP = strr.indexOf("p");
+console.log(indexOfP); 
+
+const charAt1 = strr.charAt(1);
+console.log(charAt1); 
+
+const upperCaseStr = strr.toUpperCase();
+console.log(upperCaseStr); 
+
+const lowerCaseStr = strr.toLowerCase();
+console.log(lowerCaseStr); 
