793af9ceaccc30d9874db5ac71a244607701b5b7
Tilda Källström <tildakallstrom@gmail.com>
2023-04-05 12:09:04 +0200

    Updated 2.14

--- js/ovning214.js
+++ js/ovning214.js
@@ -1,10 +1,16 @@
 function printPrefixes() {
     const text = document.getElementById("input").value;
     let output = "";
+    let output1 = "";
 
     for (let i = 1; i <= text.length; i++) {
         output += text.slice(0, i) + "<br>";
     }
 
+    for (let i = 2; i <= text.length; i++) {
+        output1 += text.slice(1, i) + "<br>";
+    }
+
     document.getElementById("output").innerHTML = output;
+    document.getElementById("output1").innerHTML = output1;
 }
