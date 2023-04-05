f394db37e15a919b03e02ca721aee109511a3f0e
Tilda Källström <tildakallstrom@gmail.com>
2023-04-05 12:43:02 +0200

    Added exercise 2.17

---
+++ js/ovning217.js
@@ -0,0 +1,8 @@
+function merryBtn() {
+    let div = document.getElementById("merry");
+    let h1 = document.createElement("h1");
+    let text = document.createTextNode("God JUL");
+    h1.appendChild(text);
+    h1.style.color = "red";
+    div.appendChild(h1);
+  }
