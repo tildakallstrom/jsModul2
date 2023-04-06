8ec539df7fc4268125206c7b6829ff639380c3b6
Tilda Källström <tildakallstrom@gmail.com>
2023-04-06 13:22:28 +0200

    Updated code

--- js/ovning218.js
+++ js/ovning218.js
@@ -1,7 +1,7 @@
 function move() {
-    var x = Math.floor(Math.random() * window.innerWidth);
-    var y = Math.floor(Math.random() * window.innerHeight);
-    var div = document.getElementById("god-jul");
+    let x = Math.floor(Math.random() * window.innerWidth);
+    let y = Math.floor(Math.random() * window.innerHeight);
+    let div = document.getElementById("god-jul");
     div.style.top = y + "px";
     div.style.left = x + "px";
 }
